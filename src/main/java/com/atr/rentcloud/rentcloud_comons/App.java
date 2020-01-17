8cf2d9ef951646961945e2e5087bbccf6210c501
Ahmed Al-Salih <ahmed.al-salih@delhaize.com>
2020-01-17 11:56:52 -0500

    Commons project for cloud rentcar project

---
+++ src/main/java/com/atr/rentcloud/rentcloud_comons/App.java
@@ -0,0 +1,13 @@
+package com.atr.rentcloud.rentcloud_comons;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/com/atr/rentcloud/rentcloud_comons/model/Customer.java
@@ -0,0 +1,63 @@
+package com.atr.rentcloud.rentcloud_comons.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Customer {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY) 
+	private int id;
+	
+	private String firstname;
+	private String lastname;
+	private String dlNumber;
+	private String zipcode;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	public String getDlNumber() {
+		return dlNumber;
+	}
+
+	public void setDlNumber(String dlUmber) {
+		this.dlNumber = dlUmber;
+	}
+
+	public String getZipcode() {
+		return zipcode;
+	}
+
+	public void setZipcode(String zipcode) {
+		this.zipcode = zipcode;
+	}
+	
+	
+	
+
+}
